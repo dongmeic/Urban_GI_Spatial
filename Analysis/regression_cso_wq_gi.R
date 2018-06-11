5a5a8d82a10b339981371e0a5f0e97a8393af6b5
dchen <dongmeic@uoregon.edu>
2018-06-11 14:07:53 -0400

    modeling

--- Analysis/regression_cso_wq_gi.R
+++ Analysis/regression_cso_wq_gi.R
@@ -9,26 +9,38 @@ library(swfscMisc)
 setwd("/nfs/urbangi-data/spatial_data/output")
 infolder <- "/nfs/urbangi-data/spatial_data/"
 
+crs <- CRS("+proj=lcc +lat_1=40.66666666666666 +lat_2=41.03333333333333
+           +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000 +y_0=0 +datum=NAD83
+           +units=us-ft +no_defs +ellps=GRS80 +towgs84=0,0,0")
+
 bound <- readOGR(dsn = paste0(infolder, "BD"), layer = "nyad_dis")
 bound <- spTransform(bound, crs)
 nyadwi <- readOGR(dsn = paste0(infolder, "BD"), layer ="nyadwi_dis")
 lonlat <- CRS("+proj=longlat +datum=NAD83")
-crs <- CRS("+proj=lcc +lat_1=40.66666666666666 +lat_2=41.03333333333333
-           +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000 +y_0=0 +datum=NAD83
-           +units=us-ft +no_defs +ellps=GRS80 +towgs84=0,0,0")
 gi_pts <- readOGR(dsn="./shapefile", layer="GIsites_all", stringsAsFactors=FALSE)
+gi_pts <- spTransform(gi_pts, lonlat)
+# check duplicated asset ID
+gi_pts[duplicated(gi_pts$Asset_ID),]@data
+# add unique ID for each GI
+gi_pts$GIid <- paste0("gi", seq(1, length(gi_pts$Asset_ID)))
+gi_pts$longtitude <- gi_pts@coords[,1]
+gi_pts$latitude <- gi_pts@coords[,2]
 gi_pts <- spTransform(gi_pts, crs)
 wbdhu12 <- readOGR(dsn = paste0(infolder, "WBDHU"), layer = "wbdhu_12", stringsAsFactors = FALSE)
 wbdhu12 <- spTransform(wbdhu12, crs)
 wbdhu12$nid <- paste0("hu", seq(1, length(wbdhu12$TNMID)))
-wq_pts <- readOGR(dsn = "./shapefile", layer = "dep_wq_sampling_sites", stringsAsFactors = FALSE)
+wq_pts <- readOGR(dsn = paste0(infolder, "WQ/DEP"), layer = "harbor_sampling_sites_priority", stringsAsFactors = FALSE)
 wq_pts <- spTransform(wq_pts, crs)
+wq_pts$hu12 <- over(wq_pts, wbdhu12)$nid
 dep_hwq_pts <- readOGR(dsn = "./shapefile", layer = "harbor_water_quality", stringsAsFactors = FALSE)
 dep_hwq_pts <- spTransform(dep_hwq_pts, crs)
 csoloc <- readOGR(dsn=paste0(infolder, "CSO"), layer ="dec_cso_2016", stringsAsFactors=FALSE) 
 csoloc <- spTransform(csoloc, crs)
-sewershed <- readOGR(dsn = paste0(infolder, "watershed/Sewershed"), layer = "Sewershed", stringsAsFactors=FALSE)
-
+#sewershed <- readOGR(dsn = paste0(infolder, "watershed/Sewershed"), layer = "Sewershed", stringsAsFactors=FALSE)
+csoshed <- readOGR(dsn = paste0(infolder, "watershed"), layer = "CSO_drainage_area", stringsAsFactors=FALSE)
+csoshed <- spTransform(csoshed, crs)
+csoshed$ID <- paste0("csoshed", seq(1, length(csoshed$OutfallAre)))
+gi_pts$csoshed <- over(gi_pts, csoshed)$ID
 # measure the distance between water quality sampling points with cso outfalls
 
 earth.dist<-function(lat1,long1,lat2,long2){
@@ -46,37 +58,49 @@ earth.dist<-function(lat1,long1,lat2,long2){
   return(dist)
 }
 
-# find the closest cso outfall
+# find the closest green infrastructure
 find.nearest.point <- function(lat, long){
   v <- vector()
-  n <- nrow(csoloc)
+  n <- nrow(gi_pts)
   for (i in 1:n){
-    v[i] <- earth.dist(lat, long, csoloc$latitude[i], csoloc$longtitude[i])
+    v[i] <- earth.dist(lat, long, gi_pts$latitude[i], gi_pts$longtitude[i])
   }
   x <- sample(which(v == min(v)), 1)
-  return(csoloc$outfall_id[x])
+  return(gi_pts$GIid[x])
 }
 
 n <- nrow(wq_pts)
 for (i in 1:n){
   #if (i %% 10 == 0)  { cat(100 * i / n, '% complete\r', sep='') }
-  wq_pts$outfall[i] <- find.nearest.point(wq_pts$Lat[i], wq_pts$Long[i])
+  wq_pts$gi[i] <- find.nearest.point(wq_pts$Lat[i], wq_pts$Long[i])
   cat("\nProcessing water quality sampling point ", i, 
-      "\nFound outfall ", wq_pts$outfall[i], "\n")
+      "\nFound green infrastructure ", wq_pts$gi[i], "\n")
 }
 
-spdf <- wq_pts[,c("Station", "outfall")]
+mode <- function(x, na.rm = FALSE) {
+  if(na.rm){
+    x = x[!is.na(x)]
+  }
+  
+  ux <- unique(x)
+  return(ux[which.max(tabulate(match(x, ux)))])
+}
+
+spdf <- wq_pts[,c("Station", "hu12")]
 names(spdf)[1] <- "site"
 dep_hwq_pts@data <- merge(dep_hwq_pts@data, spdf@data, by="site")
 # find the right sewershed for the water quality sampling points
-csoloc$sewershed <- over(csoloc, sewershed)$Sewershed
-spdf <- csoloc[,c("outfall_id", "sewershed")]
-names(spdf)[1] <- "outfall"
-dep_hwq_pts@data <- merge(dep_hwq_pts@data, spdf@data, by="outfall")
+# csoloc$sewershed <- over(csoloc, sewershed)$Sewershed
+# csoloc$csoshed <- over(csoloc, csoshed)$ID
+spdf <- gi_pts[,c("GIid", "csoshed")]
+names(spdf)[1] <- "gi"
+dep_hwq_pts@data <- merge(dep_hwq_pts@data, spdf@data, by="gi")
 
-# aggregate SGI density with sewershed
-df_gi <- aggregate(Asset_ID~sewershed, data=gi_pts, function(x) length(x))
+# aggregate SGI density with csoshed
+df_gi <- aggregate(Asset_ID~hu12, data=gi_pts, function(x) length(x))
 colnames(df_gi)[2] <- "SGI_density"
+# test
+var <- "Tra"; fun <- "mean"; unit <- "csoshed"
 reg.test <- function(var, fun, unit){
   df <- dep_hwq_pts@data %>% filter(pre2>0) %>% filter(Key==var)
   names(df)[which(names(df)==unit)] <- "unit"
@@ -84,9 +108,12 @@ reg.test <- function(var, fun, unit){
   names(df)[which(names(df)=="unit")] <- unit
   df <- merge(df_gi, df, by=unit)
   print(summary(lm(df$Value~df$SGI_density)))
+  print(cor.test(df$Value, df$SGI_density))
+  par(mfrow=c(1,1),xpd=FALSE,mar=c(4,4,2,1))
   plot(df$SGI_density, df$Value)
 }
-reg.test("DO_top", median, "sewershed")
+# value options: "Ent_top", "DO_top", "FC_top", "Tra"
+reg.test("Tra", mean, "hu12")
 
 # # merge SGI density with water quality sampling points
 # dep_hwq_pts@data <- merge(dep_hwq_pts@data, df_gi, by="sewershed")
@@ -123,7 +150,7 @@ reg.test("Tra", median, "hu12")
 # aggregate SGI density within certain radius from a water quality sampling point
 n <- nrow(wq_pts)
 for (i in 1:n){
-  circle.loc <- as.data.frame(circle.polygon(wq_pts$Long[i], wq_pts$Lat[i], radius=5, units = "km"))
+  circle.loc <- as.data.frame(circle.polygon(wq_pts$Long[i], wq_pts$Lat[i], radius=2, units = "km"))
   ch <- chull(circle.loc)
   coords <- circle.loc[c(ch, ch[1]),]
   sp_poly <- SpatialPolygons(list(Polygons(list(Polygon(coords)), ID=1)))
@@ -145,10 +172,11 @@ for (i in 1:n){
       "\nThe most common SGI type is", wq_pts$most[i], "\n")
 }
 
-spdf <- wq_pts[,c("Station", "sgi")]
+spdf <- wq_pts[,c("Station", "Priority", "hu12", "sgi", "most")]
 names(spdf)[1] <- "site"
 dep_hwq_pts@data <- merge(dep_hwq_pts@data, spdf@data, by="site")
 
+# add another two variables
 # closest distance to one SGI and the SGI type
 nearest.gi <- function(lat, long){
   v <- vector()
@@ -171,15 +199,36 @@ for (i in 1:n){
 }
 
 head(wq_pts)
-spdf <- wq_pts[,c("Station", "sgi", "dist", "type", "most")]
+spdf <- wq_pts[,c("Station", "dist", "type")]
 names(spdf)[1] <- "site"
-dep_hwq_pts@data <- merge(dep_hwq_pts@data, spdf@data, by="site")
+hwq.df <- merge(dep_hwq_pts@data, spdf@data, by="site")
 
-df <- dep_hwq_pts@data %>% filter(pre2 > 0 & Key == "DO_top")
-hist(log(df$Value))
+df <- dep_hwq_pts@data %>% filter(pre2 > 0 & Key == "Tra" & !is.na(Value))
+#df <- hwq.df %>% filter(pre2 > 0 & Key == "Tra")
+#hist(log(df$Value))
+hist(df$Value)
+dim(df)
+# model with latitude, longitude, year, month, precipitation, SGI density, 
+# distance to the closest density, the closest SGI type, the most frequent SGI type
+# trials in modeling
 model <- lm(log(Value+1)~Lat*Long+as.factor(year)+as.factor(month)+pre7+sgi+dist+type+most, 
              data=df)
+model <- lm(Value~Lat*Long+pre2+sgi+dist+type+most,data=df)
+model <- lm(Value~Lat*Long+as.factor(year)+pre2+Priority+hu12+sgi+most,data=df)
+
 model <- step(model)
 summary(model)
 par(mfrow=c(2, 2))
 plot(model)
+par(mfrow=c(1, 1))
+plot(df$sgi, df$Value)
+
+# model with SGI density, mitigated area, ID of the hydrologic unit, binary variable with priority watershed
+# pre2, year as factor
+
+PriorityCSOWatershed <- readOGR(dsn=paste0(infolder,"watershed"),
+                                layer="priority_cso_watersheds", stringsAsFactors = FALSE)
+PriorityCSOWatershed <- spTransform(PriorityCSOWatershed, crs)
+mitigated_area <- readOGR(dsn="./shapefile", layer ="DEP_GI_withDA_040618", stringsAsFactors=FALSE)
+mitigated_area <- spTransform(mitigated_area, crs)
+
