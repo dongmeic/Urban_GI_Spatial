765fec11431326759ce87fde078dbabb02db5e31
dchen <dongmeic@uoregon.edu>
2018-08-21 19:30:57 -0400

    Added mitigated imperviousness, transparency, and equity index to area pattern analysis

--- Analysis/Area_Pattern_Analysis.R
+++ Analysis/Area_Pattern_Analysis.R
@@ -80,6 +80,9 @@ wq.spdf <- spTransform(wq.df, crs)
 wq.spdf.u <- remove.duplicates(wq.spdf)
 hwq.spdf <- readOGR(dsn="./shapefile", layer="harbor_water_quality", stringsAsFactors = FALSE)
 hwq.spdf <- spTransform(hwq.spdf, crs)
+equity_index <- readOGR(dsn="/nfs/urbangi-data/spatial_data/Stewardship Map/GIS Data", 
+                        layer="EquityIndex_022218", stringsAsFactors = FALSE)
+equity_index <- spTransform(equity_index, crs)
 
 sewershed <- readOGR(dsn = paste0(infolder, "CSO"), layer ="Sewershed", stringsAsFactors = FALSE)
 #reference: https://gis.stackexchange.com/questions/163445/r-solution-for-topologyexception-input-geom-1-is-invalid-self-intersection-er
@@ -105,19 +108,23 @@ points(wq.spdf, pch=20, col='blue')
 # pts.poly@data$pids <- 1:nrow(pts.poly) 
 # head(pts.poly@data)
 ## use over function instead
-#pts.poly <- over(wq.spdf, nyccwi[,"CounDist"])
-pts.poly <- over(hwq.spdf, nyccwi[,"CounDist"])
+wqpts.poly <- over(wq.spdf, nyccwi[,"CounDist"])
+hwqpts.poly <- over(hwq.spdf, nyccwi[,"CounDist"])
+equity_index.in <- over(equity_index, nyccwi[,"CounDist"])
+
 gi.spdf <- readOGR(dsn="/nfs/urbangi-data/spatial_data/GI", layer="GIsites_all_noreplicates_033118", stringsAsFactors=FALSE)
 gi.spdf <- spTransform(gi.spdf, crs)
 gi.pts.poly <- over(gi.spdf, nyccwi[,"CounDist"])
 mitigated_area <- readOGR(dsn="./shapefile", layer ="DEP_GI_withDA_040618", stringsAsFactors=FALSE)
 mitigated_area <- spTransform(mitigated_area, crs)
 mitigated_area.pts <- over(mitigated_area, nyccwi[,"CounDist"])
-#wq.spdf$CounDist <- pts.poly$CounDist
-hwq.spdf$CounDist <- pts.poly$CounDist
+wq.spdf$CounDist <- wqpts.poly$CounDist
+hwq.spdf$CounDist <- hwqpts.poly$CounDist
 gi.spdf$CounDist <- gi.pts.poly$CounDist
+equity_index$CounDist <- equity_index.in$CounDist
+
 mitigated_area$CounDist <- mitigated_area.pts$CounDist
-#wq.spdf$pids <- 1:nrow(wq.spdf)
+wq.spdf$pids <- 1:nrow(wq.spdf)
 hwq.spdf$pids <- 1:nrow(hwq.spdf)
 gi.spdf$pids <- 1:nrow(gi.spdf)
 # aggregate data by polygon
@@ -125,14 +132,19 @@ cpts <- aggregate(pids ~ CounDist, data = wq.spdf@data, FUN = function(x){y <- l
 colnames(cpts)[2] <- "cpts"
 gicpts <- aggregate(pids ~ CounDist, data = gi.spdf@data, FUN = function(x){y <- length(x); return(y)})
 colnames(gicpts)[2] <- "gicpts"
-ment <- aggregate(ent ~ CounDist, data = wq.spdf@data, FUN = mean)
-mitiarea <- aggregate(mtgtn_2 ~ CounDist, data = mitigated_area@data, FUN = sum)
-hwq <- aggregate(Value ~ CounDist, data = subset(hwq.spdf, Key=="Tra"), FUN = mean)
+ment <- aggregate(ent ~ CounDist, data = wq.spdf@data, FUN = mean, na.rm=TRUE)
+mitiarea <- aggregate(mtgtn_2 ~ CounDist, data = mitigated_area@data, FUN = sum, na.rm=TRUE)
+hwq.df <- subset(hwq.spdf@data, Key=="Tra")
+hwq.df <- hwq.df[!is.na(hwq.df$Value),]
+hwq <- aggregate(Value ~ CounDist, data = hwq.df, FUN = mean, na.rm=TRUE)
+equity.index <- aggregate(GIq_ndx ~ CounDist, data = equity_index, FUN = mean, na.rm=TRUE)
+
 nyccwi@data <- merge(nyccwi@data, cpts, by="CounDist", all=T)
 nyccwi@data <- merge(nyccwi@data, ment, by="CounDist", all=T)
 nyccwi@data <- merge(nyccwi@data, gicpts, by="CounDist", all=T)
 nyccwi@data <- merge(nyccwi@data, mitiarea, by="CounDist", all=T)
 nyccwi@data <- merge(nyccwi@data, hwq, by="CounDist", all=T)
+nyccwi@data <- merge(nyccwi@data, equity.index, by="CounDist", all=T)
 # remove repeated
 # nyccwi@data <- nyccwi@data[, -which(names(df) %in% c("gicpts.x","gicpts.y"))]
 #write.csv(nyccwi@data, "wq_data_cc.csv", row.names=FALSE)
@@ -156,11 +168,17 @@ gidens.lagged.means <- lag.listw(nyccwi.lw, nyccwi$gidens, NAOK=TRUE)
 # replace NAs with zeros?
 #nyccwi$ent[is.na(nyccwi$ent)] <- 0
 #nyccwi$ent[nyccwi$ent==0] <- NA
+nyccwi$ent[is.na(nyccwi$ent)] <- 0
 shades <- auto.shading(nyccwi$ent, n=6, cols = brewer.pal(5, 'Blues'))
 choropleth(nyccwi, nyccwi$ent, shades)
 
+nyccwi$gidens[is.na(nyccwi$gidens)] <- 0
 shades <- auto.shading(nyccwi$gidens, n=6, cols = brewer.pal(5, 'Blues'))
 choropleth(nyccwi, nyccwi$gidens, shades)
+
+nyccwi$mtgtn_2[is.na(nyccwi$mtgtn_2)] <- 0
+shades <- auto.shading(nyccwi$mtgtn_2, n=6, cols = brewer.pal(5, 'Blues'))
+choropleth(nyccwi, nyccwi$mtgtn_2, shades)
 
 par(xpd=FALSE,mfrow=c(1,1),mar=c(4.5,4.5,2.5,0.5))
 plot(nyccwi$ent, ent.lagged.means, asp=1, xlim=range(na.omit(nyccwi$ent)), ylim=range(na.omit(nyccwi$ent)))
@@ -178,25 +196,70 @@ moran.test(nyccwi$ent, nyccwi.lw, na.action=na.exclude, zero.policy = TRUE)
 # Moran I test under randomisation
 # 
 # data:  nyccwi$ent  
-# weights: nyccwi.lw 
-# omitted: 2, 3, 4, 5, 11, 12, 13, 15, 18, 20, 21, 22, 24, 34, 35, 39, 41, 44 
+# weights: nyccwi.lw    
 # 
-# Moran I statistic standard deviate = -0.30781, p-value = 0.6209
+# Moran I statistic standard deviate = 0.11942, p-value = 0.4525
 # alternative hypothesis: greater
 # sample estimates:
 #   Moran I statistic       Expectation          Variance 
-# -0.06538677       -0.03125000        0.01229916 
+# -0.01132845       -0.02000000        0.00527296
 
 moran.test(nyccwi$gidens, nyccwi.lw, na.action=na.exclude, zero.policy = TRUE)
+# Moran I test under randomisation
+# 
+# data:  nyccwi$gidens  
+# weights: nyccwi.lw    
+# 
+# Moran I statistic standard deviate = 0.85261, p-value = 0.1969
+# alternative hypothesis: greater
+# sample estimates:
+#   Moran I statistic       Expectation          Variance 
+# 0.052230128      -0.020000000       0.007176918 
+
+moran.test(nyccwi$mtgtn_2, nyccwi.lw, na.action=na.exclude, zero.policy = TRUE)
+# Moran I test under randomisation
+# 
+# data:  nyccwi$mtgtn_2  
+# weights: nyccwi.lw    
+# 
+# Moran I statistic standard deviate = 0.61459, p-value = 0.2694
+# alternative hypothesis: greater
+# sample estimates:
+#   Moran I statistic       Expectation          Variance 
+# 0.029948799      -0.020000000       0.006605087 
 
 # LISA
 nyccwi.lI <- localmoran(nyccwi$ent, nyccwi.lw, na.action=na.exclude, zero.policy = TRUE)
+nyccwi.lI <- localmoran(nyccwi$Value, nyccwi.lw, na.action=na.exclude, zero.policy = TRUE)
 nyccwi.lI <- localmoran(nyccwi$gidens, nyccwi.lw, na.action=na.exclude, zero.policy = TRUE)
+nyccwi.lI <- localmoran(nyccwi$mtgtn_2, nyccwi.lw, na.action=na.exclude, zero.policy = TRUE)
+nyccwi.lI <- localmoran(nyccwi$GIq_ndx, nyccwi.lw, na.action=na.exclude, zero.policy = TRUE)
 
 # mapping water quality
-png(paste("figure/water_quality.png", sep=""), width=9, height=8, units="in", res=300)
+png(paste("figure/water_quality_ent.png", sep=""), width=9, height=8, units="in", res=300)
 par(xpd=FALSE,mfrow=c(1,1),mar=c(0.5,0.5,2.5,0.5))
-mapping(nyccwi, 'Blues', "ent", 8, "Enterococci", "Water quality in New York City")
+mapping(nyccwi, 'Blues', "ent", 8, "Enterococci", "Harbor water quality in New York City")
+add.northarrow()
+add.scale()
+dev.off()
+
+png(paste("figure/water_quality_tra.png", sep=""), width=9, height=8, units="in", res=300)
+par(xpd=FALSE,mfrow=c(1,1),mar=c(0.5,0.5,2.5,0.5))
+mapping(nyccwi, 'Blues', "Value", 8, "Transparency", "Harbor water quality in New York City")
+add.northarrow()
+add.scale()
+dev.off()
+
+png(paste("figure/mitigated_area.png", sep=""), width=9, height=8, units="in", res=300)
+par(xpd=FALSE,mfrow=c(1,1),mar=c(0.5,0.5,2.5,0.5))
+mapping(nyccwi, 'Blues', "mtgtn_2", 8, "Mitigated area", "Mitigated imperviousness")
+add.northarrow()
+add.scale()
+dev.off()
+
+png(paste("figure/equity_index.png", sep=""), width=9, height=8, units="in", res=300)
+par(xpd=FALSE,mfrow=c(1,1),mar=c(0.5,0.5,2.5,0.5))
+mapping(nyccwi, 'Blues', "GIq_ndx", 8, "Equity index", "Equity index")
 add.northarrow()
 add.scale()
 dev.off()
@@ -215,6 +278,24 @@ nyccwi_lI$lisa <- nyccwi.lI[,1]
 nyccwi_lI$pvalue <- nyccwi.lI[,5]
 png(paste("figure/local_moran_I_pvalue_gi.png", sep=""), width=10, height=6, units="in", res=300)
 par(xpd=FALSE,mfrow=c(1,2),mar=c(0.5,0.5,2.5,0.5))
-mapping(nyccwi_lI, 'PRGn', "lisa", 5, "Local Moran's I", "Local Moran's I")
-mapping(nyccwi_lI, 'PuRd', "pvalue", 5, "Local p-value", "Local p-value")
+mapping(nyccwi_lI, 'PRGn', "lisa", 5, "Local Moran's I", "Green infrastructure density")
+mapping(nyccwi_lI, 'PuRd', "pvalue", 5, "Local p-value", "Green infrastructure density")
+dev.off()
+
+png(paste("figure/local_moran_I_pvalue_tra.png", sep=""), width=10, height=6, units="in", res=300)
+par(xpd=FALSE,mfrow=c(1,2),mar=c(0.5,0.5,2.5,0.5))
+mapping(nyccwi_lI, 'PRGn', "lisa", 5, "Local Moran's I", "Transparency")
+mapping(nyccwi_lI, 'PuRd', "pvalue", 5, "Local p-value", "Transparency")
+dev.off()
+
+png(paste("figure/local_moran_I_pvalue_miti.png", sep=""), width=10, height=6, units="in", res=300)
+par(xpd=FALSE,mfrow=c(1,2),mar=c(0.5,0.5,2.5,0.5))
+mapping(nyccwi_lI, 'PRGn', "lisa", 5, "Local Moran's I", "Mitigated imperviousness")
+mapping(nyccwi_lI, 'PuRd', "pvalue", 5, "Local p-value", "Mitigated imperviousness")
+dev.off()
+
+png(paste("figure/local_moran_I_pvalue_equity.png", sep=""), width=10, height=6, units="in", res=300)
+par(xpd=FALSE,mfrow=c(1,2),mar=c(0.5,0.5,2.5,0.5))
+mapping(nyccwi_lI, 'PRGn', "lisa", 5, "Local Moran's I", "Equity index")
+mapping(nyccwi_lI, 'PuRd', "pvalue", 5, "Local p-value", "Equity index")
 dev.off()
